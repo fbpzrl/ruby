509180804f26ab2861603e4945d528d506ffeab6
Fabio Piazzarollo <fbpzrl@gmail.com>
2021-12-24 14:40:58 -0300

    Teste Drive Virtual

---
+++ testeDriveVirtual.rb
@@ -0,0 +1,23 @@
+# Got-A-Motor, Inc - Teste Drive Virtual
+# Escrito por: Fabio Piazzarollo
+
+def acelerar
+    puts "Pisando no acelerador"
+    puts "Acelerando"
+end
+
+def buzinar
+    puts "Pressionando a buzina"
+    puts "Beep beep!"
+end
+
+def usarFarois (brilho)
+    puts "Acender farol #{brilho}"
+    puts "Cuidado com os animais!"
+end
+
+buzinar
+acelerar
+usarFarois("alto")
+
+
