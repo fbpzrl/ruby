4e81d606c53cd0643e32e5c86f331ec612e7dce8
Fabio Piazzarollo <fbpzrl@gmail.com>
2023-05-14 19:51:06 -0300

    Casting - Adição de comentarios

--- Ruby e OO/04-casting.rb
+++ Ruby e OO/04-casting.rb
@@ -2,3 +2,10 @@ puts "Digite sua idade:"
 v1 = gets.chomp
 v2 = v1.to_i + 1
 puts "Sua idade ano que vem será #{v2} anos."
+
+=begin
+Principais castings (conversões):
+to_i : Converte para Inteiro
+to_f : Converte para Float
+to_s : Converte para String
+=end
